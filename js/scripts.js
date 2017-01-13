8e6f94fb75fe2760834b10c59d633542b7796e30
Alexandra Holcombe and Grinil Khanna <grinil@>
2017-01-12 16:36:53 -0800

    add working isoceles, triangle, and not a triangle results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,41 @@
 $(document).ready(function() {
   $("#blanks").submit(function(event) {
+    $(".result").hide();
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
-    var side3 = parseInt($("input#Side3").val());
-    var sum =
+    var side3 = parseInt($("input#side3").val());
+    console.log("1: " + side1);
+    console.log("2: " + side2);
+    console.log("3: " + side3);
 
+    // if ((side1 === side2) && (side1 === side3)) {
+    //   alert("equilateral");
+    // } else {
+    //   alert("done");
+    // }
 
-if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) 
+    if (((side1 + side2) <= side3) || ((side1 + side3) <= side2) || ((side2 + side3) <= side1)) {
+      $("#neg-result").show();
+      alert("Triggered not-a-triangle");
+    } else {
+      if ((side1 === side2) && (side2 === side3)) {
+        $("#triangle").text("an equilateral");
+        $("#pos-result").show();
+      } else if ((side1 === side2) && (side1 !== side3)) {
+        $("#triangle").text("an isosceles");
+        $("#pos-result").show();
+      } else {
+        alert(done);
+      };
+      // } else if ((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
+      //   $("#triangle").text("a scalene");
+      //   $("pos-result").show();
+      // } else {
+        // $("#triangle").text("an isosceles");
+        // $("pos-result").show();
+      // };
+    };
 
-if (side1 === side2 === side3) {
-  $(".result").text("an equilateral");
-}
-else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
-    $(".result").text("an isoceles");
-}
-else () {
-
-}
+    event.preventDefault();
   });
 });
